c44f66f98923f8cc30a2d065b2e03e0520f6dcfd
Brendon stead <brendon_stead@yahoo.com>
2024-05-01 16:04:24 +0100

    profile

--- SRC/seo_wireframe/style1/urls.py
+++ SRC/seo_wireframe/style1/urls.py
@@ -4,6 +4,8 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('landing1/', TemplateView.as_view(template_name='style1/landing.html'), name='landing1'),
     path('profile1/', TemplateView.as_view(template_name='style1/profile.html'), name='profile1'),
+    path('profile1-1/', TemplateView.as_view(template_name='style1/profile1-1.html'), name='profile1-1'),
     path('dash/', TemplateView.as_view(template_name='style1/dashboard.html'), name='dashboard'),
     path('alumni/', TemplateView.as_view(template_name='style1/alumni.html'), name='alumni1'),
+    path('events/', TemplateView.as_view(template_name='style1/events.html'), name='events1'),
 ]
